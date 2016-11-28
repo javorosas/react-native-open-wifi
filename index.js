d171dd44b88213fba75fa973a64a5bfa7c1054da
Javier Rosas <javier.rosasb@gmail.com>
2016-11-27 21:48:58 -0700

    make connect method return a promise on completion

--- index.js
+++ index.js
@@ -2,6 +2,44 @@
  * @providesModule react-native-open-wifi
  */
 
-var { OpenWifi } = require('react-native').NativeModules;
+var { RNOpenWifi } = require('react-native').NativeModules;
+
+var OpenWifi = {
+  connect: (ssid) => {
+    return Promise.race([timeOut(), connectToSsid(ssid)]);
+  }
+};
+
+const connectToSsid = (ssid) => {
+  return new Promise((resolve, reject) => {
+    var { NetInfo } = require('react-native');
+    // Connect to WiFi network
+    RNOpenWifi.connect(ssid);
+
+    NetInfo.addEventListener('change', networkChanged);
+
+    function networkChanged (state) {
+      console.log(state);
+      if (/^wi/i.test(state)) {
+        NetInfo.removeEventListener('change', networkChanged);
+        RNOpenWifi.getSSID(function (currentSsid) {
+          console.log(currentSsid);
+          if (currentSsid === ssid) {
+            resolve();
+          } else {
+            reject(new Error('Couldn\'t find the network'));
+          }
+        });
+      }
+    }
+  });
+};
+
+const ONE_SECOND = 1000;
+const timeOut = (milliseconds = 20 * ONE_SECOND) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(reject, milliseconds, new Error('Timeout!'));
+  });
+};
 
 module.exports = OpenWifi;
